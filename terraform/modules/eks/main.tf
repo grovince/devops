3ef5cd786cf5eb4529692c37e18c2616a87bd6d3
grovince <grovinceln@gmail.com>
2024-11-13 12:42:13 +0900

    eks with terraform

---
+++ terraform/modules/eks/main.tf
@@ -0,0 +1,42 @@
+locals {
+    node_group_name        = "${var.cluster_name}-node-group"
+    iam_role_policy_prefix = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy"
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      Terraform = "true"
+      Project   = "${var.cluster_name}-project"
+    }
+  }
+}
+
+module "eks"{
+    source = "terraform-aws-modules/eks/aws"
+
+    cluster_name    = var.cluster_name
+    cluster_version = var.cluster_version
+
+    cluster_endpoint_public_access  = true
+
+    vpc_id          = module.network.vpc_id
+    subnet_ids      = module.network.public_subnet_ids
+
+    eks_managed_node_group_defaults = {
+        instance_types = ["t2.medium"]
+    }
+
+    eks_managed_node_groups = {
+        one = {
+            name 
+            ami_type       = "AL2_x86_64"
+            instance_types = ["t2.medium"]
+
+            min_size     = 2
+            max_size     = 4
+            desired_size = 2
+    }
+}
--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,3 +1,8 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value = aws_vpc.this.id
+}
+
 output "igw_id" {
   description = "The ID of the Internet Gateway"
   value       = aws_internet_gateway.igw.id
