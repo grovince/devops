44d9b77b92b472aa1638ff39ab83fa3dadd5cc9c
grovince <grovinceln@gmail.com>
2025-03-04 09:16:24 +0900

    [Delete]: .gitignore 파일 추가

--- terraform/modules/IAM/iam-eks-role.tf
+++
@@ -1,9 +0,0 @@
-module "iam_eks_role" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-eks-role"
-
-  role_name = "my-app"
-
-  cluster_service_accounts = {
-    "cluster1" = ["default:322084995268"]
-  }
-}
