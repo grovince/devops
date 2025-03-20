cfe0e519cdefaf524a272f75c74445575c77efe5
grovince <grovinceln@gmail.com>
2025-03-20 22:00:11 +0900

    [Feat]: eks 생성에 필요한 IAM 정책 추가

--- terraform/policy.tf
+++ terraform/policy.tf
@@ -1,3 +1,67 @@
+# EKS 클러스터 역할
+resource "aws_iam_role" "devops" {
+  name = "eks-cluster-devops"
+  tags = {
+    tag-key = "eks-cluster-devops"
+  }
+
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": [
+                    "eks.amazonaws.com"
+                ]
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+POLICY
+}
+
+# EKS 클러스터 역할에 정책 연결
+resource "aws_iam_role_policy_attachment" "devops-AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.devops.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# 노드 IAM 역할
+resource "aws_iam_role" "nodes" {
+  name = "eks-node-group-nodes"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+# 노드 IAM 역할에 정책 연결
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
+
+# EBS CSI 드라이버를 위한 IAM 역할 및 정책
 data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
@@ -7,19 +71,8 @@ module "irsa-ebs-csi" {
   version = "4.7.0"
 
   create_role                   = true
-  role_name                     = "AmazonEKSEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
+  role_name                     = "AmazonEKSTFEBSCSIRole-${aws_eks_cluster.devops.name}"
+  provider_url                  = aws_eks_cluster.devops.identity.0.oidc.0.issuer
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.20.0-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
-  }
 }
